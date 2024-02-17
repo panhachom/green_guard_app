b40dac1164acb98d193b8c6ec05cd825eaf1ce86
panhachom <panhachom@gmail.com>
2024-02-17 14:16:51 +0700

    add url helper

--- lib/constraint/helper.dart
+++ lib/constraint/helper.dart
@@ -2,4 +2,6 @@ import 'package:flutter/animation.dart';
 
 class Helper {
   static const Color primaryColor = Color(0xFF4CAF50);
+  static const String developmentUrl = 'http://127.0.0.1:8000'; 
+  static const String productionUrl = 'https://greenguard-production.up.railway.app';
 }
